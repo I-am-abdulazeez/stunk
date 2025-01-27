8979ea4d16f88bbdc4b14aaa45c995c740ea419e
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-01-27 18:07:46 +0100

    feat: add optional derive method to Chunk interface for value transformation

--- types/stunk.d.ts
+++ types/stunk.d.ts
@@ -5,6 +5,7 @@ declare module 'stunk' {
     get: () => T;
     set: (value: T) => void;
     subscribe: (callback: Subscriber<T>) => () => void;
+    derive?: <D>(fn: (value: T) => D) => Chunk<D>;
   }
 
   export function chunk<T>(initialValue: T): Chunk<T>;
