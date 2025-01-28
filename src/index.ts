c3b7a0c76156c96c57c8e83e0d98c5a113a6d89a
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-01-28 17:48:07 +0100

    feat: export logger and nonNegativeValidator from middleware

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
 export { chunk } from './core';
 export type { Chunk } from './core';
+
+export { logger } from "./middleware/logger";
+export { nonNegativeValidator } from "./middleware/validator";
