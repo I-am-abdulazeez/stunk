e84b36f4e913563ff7e29d006dd5142856561a74
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-01-31 07:55:00 +0100

    refactor: simplify exports in index.ts to aggregate all middleware components

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,4 @@
 export { chunk } from './core/core';
 export type { Chunk } from './core/core';
 
-export { logger } from "./middleware/logger";
-export { nonNegativeValidator } from "./middleware/validator";
-
-export { withHistory } from './middleware/history';
+export * from "./middleware";
