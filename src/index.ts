abb5be3eec4262a6ac2099887e8d9b5cefbba0f4
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-02-11 16:15:43 +0100

    feat: export computed function from core module

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export { chunk, batch, select } from './core/core';
 export { asyncChunk } from './core/asyncChunk'
+export { computed } from './core/computed'
 export type { Chunk, Middleware } from './core/core';
 
 export * from "./middleware";
