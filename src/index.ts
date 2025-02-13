dd4410a1530de712d3be97820de484a45660897d
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-02-13 14:49:38 +0100

    feat: add select and combineAsyncChunks exports to index

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
-export { chunk, batch, select } from './core/core';
-export { asyncChunk } from './core/asyncChunk'
-export { computed } from './core/computed'
+export { chunk, batch } from './core/core';
+export { asyncChunk } from './core/asyncChunk';
+export { computed } from './core/computed';
+export { select } from './core/selector'
+
+export { combineAsyncChunks } from './utils';
+
 export type { Chunk, Middleware } from './core/core';
 
 export * from "./middleware";
