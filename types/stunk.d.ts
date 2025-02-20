1ad2fd46dbf5022931093100fbe2bec8767a9b4f
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-02-20 21:18:51 +0100

    feat: add update, reset, and destroy methods to Chunk interface

--- types/stunk.d.ts
+++ types/stunk.d.ts
@@ -4,8 +4,11 @@ declare module 'stunk' {
   export interface Chunk<T> {
     get: () => T;
     set: (value: T) => void;
+    update: (updater: (currentValue: T) => T) => void;
     subscribe: (callback: Subscriber<T>) => () => void;
-    derive?: <D>(fn: (value: T) => D) => Chunk<D>;
+    derive: <D>(fn: (value: T) => D) => Chunk<D>;
+    reset: () => void;
+    destroy: () => void;
   }
 
   export function chunk<T>(initialValue: T): Chunk<T>;
