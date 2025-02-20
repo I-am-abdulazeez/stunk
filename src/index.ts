a009cd54074032eec5f10c9ab486764dadc7aacb
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-02-20 21:19:19 +0100

    feat: export additional utilities and restructure middleware exports in index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,8 @@ export { asyncChunk } from './core/asyncChunk';
 export { computed } from './core/computed';
 export { select } from './core/selector'
 
-export { combineAsyncChunks } from './utils';
+export { combineAsyncChunks, once, isChunk, isValidChunkValue } from './utils';
 
 export type { Chunk, Middleware } from './core/core';
 
-export * from "./middleware";
+export * as middleware from "./middleware";
