e0fa2e0c503da689aeedf83cc7227d1378424f31
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-02-21 21:15:34 +0100

    feat: optimize useDerive hook by memoizing derived chunk calculation

--- src/use-react/hooks/useDerive.ts
+++ src/use-react/hooks/useDerive.ts
@@ -1,8 +1,11 @@
-import { useChunk } from "./useChunk";
+import { useMemo } from "react";
 
+import { useChunk } from "./useChunk";
 import type { Chunk } from "../../core/core";
 
 export function useDerive<T, D>(chunk: Chunk<T>, fn: (value: T) => D): D {
-  const [derivedValue] = useChunk(chunk.derive(fn));
+  const derivedChunk = useMemo(() => chunk.derive(fn), [chunk, fn]);
+  const [derivedValue] = useChunk(derivedChunk);
+
   return derivedValue;
 }
