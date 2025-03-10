212fe61c98e64ded1efd414335b22f5dedadbf8e
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-03-10 18:15:14 +0100

    refactor: select function to use computed for improved efficiency and readability

--- src/core/selector.ts
+++ src/core/selector.ts
@@ -1,27 +1,12 @@
-import { chunk, Chunk } from "./core";
+import { Chunk } from "./core";
+import { computed } from "./computed";
+
 
 export function select<T, S>(sourceChunk: Chunk<T>, selector: (value: T) => S): Chunk<S> {
-  const initialValue = selector(sourceChunk.get());
-  const selectedChunk = chunk(initialValue);
-  let previousSelected = initialValue;
-
-  // Subscribe to source changes with equality checking
-  sourceChunk.subscribe((newValue) => {
-    const newSelected = selector(newValue);
-
-    // Only update if the selected value actually changed
-    if (!Object.is(newSelected, previousSelected)) {
-      previousSelected = newSelected;
-      selectedChunk.set(newSelected);
-    }
-  });
-
-  // Return read-only version of the chunk
   return {
-    ...selectedChunk,
-    // Prevent setting values directly on the selector
+    ...computed([sourceChunk], (value) => selector(value)),
     set: () => {
       throw new Error('Cannot set values directly on a selector. Modify the source chunk instead.');
-    }
+    },
   };
 }
