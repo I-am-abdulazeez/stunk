0518b2430ec3bcbabe034bdd6f272784f429146f
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-03-22 13:55:50 +0100

    feat: enhance select function with caching and unsubscribe mechanism

--- src/core/selector.ts
+++ src/core/selector.ts
@@ -1,12 +1,35 @@
-import { Chunk } from "./core";
-import { computed } from "./computed";
-
+import { Chunk, chunk } from "./core";
+import { shallowEqual } from "../utils";
 
 export function select<T, S>(sourceChunk: Chunk<T>, selector: (value: T) => S): Chunk<S> {
+  let cachedValue = selector(sourceChunk.get());
+  const selectorChunk = chunk(cachedValue);
+
+  const update = () => {
+    const newSourceValue = sourceChunk.get();
+    const newValue = selector(newSourceValue);
+    const isObject = typeof newValue === 'object' && newValue !== null;
+    const isCachedObject = typeof cachedValue === 'object' && cachedValue !== null;
+    if (
+      (isObject && isCachedObject && !shallowEqual(newValue, cachedValue)) ||
+      (!isObject && !isCachedObject && newValue !== cachedValue)
+    ) {
+      cachedValue = newValue;
+      selectorChunk.set(newValue);
+    }
+  };
+
+  const unsubscribe = sourceChunk.subscribe(update);
+
   return {
-    ...computed([sourceChunk], (value) => selector(value)),
+    ...selectorChunk,
+    get: () => cachedValue,
     set: () => {
       throw new Error('Cannot set values directly on a selector. Modify the source chunk instead.');
     },
+    destroy: () => {
+      unsubscribe();
+      selectorChunk.destroy();
+    }
   };
 }
