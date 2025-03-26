7c1f3a5f8d71c21c5aec67a3e4c30a9f57edfe0b
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-03-26 17:14:02 +0100

    chore: remove AsyncChunkOpt type definition to streamline type management

--- src/core/types.ts
+++ src/core/types.ts
@@ -1,15 +1,9 @@
 import { AsyncChunk } from "./asyncChunk";
 
-export type AsyncChunkOpt<T, E extends Error> = {
-  initialData?: T | null;
-  onError?: (error: E) => void;
-  retryCount?: number;
-  retryDelay?: number;
-}
-
 export type InferAsyncData<T> = T extends AsyncChunk<infer U, Error> ? U : never;
 
 export type CombinedData<T> = { [K in keyof T]: InferAsyncData<T[K]> | null };
+
 export type CombinedState<T> = {
   loading: boolean;
   error: Error | null;
