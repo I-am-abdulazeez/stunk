8a221331ffb57f34b2b160e8b3fb99b2415d19c0
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-10-18 03:41:58 +0100

    chore(middleware): fix type issue, and correct imports

--- src/middleware/validator.ts
+++ src/middleware/validator.ts
@@ -1,8 +1,8 @@
-import { chunk, Middleware } from "../core/core";
+import type { Middleware } from "../core/core";
 
 export const nonNegativeValidator: Middleware<number> = (value, next) => {
   if (value < 0) {
     throw new Error("Value must be non-negative!");
   }
-  next(value); // If validation passes, proceed with the update
+  next(value);
 };
