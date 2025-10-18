daedec24dd0ca86621ef93b7d9c879892af67d5c
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-10-18 03:16:50 +0100

    chore: export query as a seperate module

---
+++ src/query/index.ts
@@ -0,0 +1,4 @@
+export type { AsyncChunk, PaginatedAsyncChunk, AsyncState, AsyncStateWithPagination } from '../query/async-chunk';
+
+export { asyncChunk } from './async-chunk';
+export { infiniteAsyncChunk } from './infinite-async-chunk';
