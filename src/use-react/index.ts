eebd2922d65694728e341674f2f78b90e8eb19da
AbdulAzeez <abdulazeezolamide50@gmail.com>
2026-03-13 12:25:21 +0100

    chore(test): remove useChunkValues()

--- src/use-react/index.ts
+++ src/use-react/index.ts
@@ -1,6 +1,5 @@
 export { useChunk } from './hooks/use-chunk'
 export { useChunkValue } from './hooks/use-chunk-value'
-export { useChunkValues } from './hooks/use-chunk-values'
 
 export { useAsyncChunk } from './hooks/use-async-chunk'
 export { useInfiniteAsyncChunk } from './hooks/use-infinite-async-chunk'
