8745312ca735773dca394bac790ae9fb4a79303f
AbdulAzeez <abdulazeezolamide50@gmail.com>
2026-03-19 18:06:45 +0100

    feat(react): export useMutation and UseMutationResult from stunk/react

--- src/use-react/index.ts
+++ src/use-react/index.ts
@@ -3,3 +3,6 @@ export { useChunkValue } from './hooks/use-chunk-value'
 
 export { useAsyncChunk } from './hooks/use-async-chunk'
 export { useInfiniteAsyncChunk } from './hooks/use-infinite-async-chunk'
+
+export { useMutation } from './hooks/use-mutation';
+export type { UseMutationResult } from './hooks/use-mutation';
