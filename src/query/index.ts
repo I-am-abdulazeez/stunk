8f21139cd3f375fc6d408ac392779fe972fd34bb
AbdulAzeez <abdulazeezolamide50@gmail.com>
2026-03-19 16:40:50 +0100

    feat(query): export mutation, MutationOptions, MutationState, MutationResult, MutationFn from stunk/query

--- src/query/index.ts
+++ src/query/index.ts
@@ -6,3 +6,6 @@ export { combineAsyncChunks } from "./combine-async-chunk";
 
 export { configureQuery, getGlobalQueryConfig, resetQueryConfig } from './configure-query';
 export type { GlobalQueryConfig } from './configure-query';
+
+export { mutation } from './mutation';
+export type { Mutation, MutationOptions, MutationState, MutationResult, MutationFn } from './mutation';
