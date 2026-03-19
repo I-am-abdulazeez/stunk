6c72a62f0d8cc88cecacd498be1b3414c9d7b0e2
AbdulAzeez <abdulazeezolamide50@gmail.com>
2026-03-19 12:54:35 +0100

    chore(query): export as module for configs

--- src/query/index.ts
+++ src/query/index.ts
@@ -3,3 +3,6 @@ export type { AsyncChunk, PaginatedAsyncChunk, AsyncState, AsyncStateWithPaginat
 export { asyncChunk } from './async-chunk';
 export { infiniteAsyncChunk } from './infinite-async-chunk';
 export { combineAsyncChunks } from "./combine-async-chunk";
+
+export { configureQuery, getGlobalQueryConfig, resetQueryConfig } from './configure-query';
+export type { GlobalQueryConfig } from './configure-query';
